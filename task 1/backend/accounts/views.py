b01f7ac4f04972e0ae52917d6cee05ea19c7d4b5
Ram Karthi <karthi51216@gmail.com>
2026-04-09 23:03:35 +0530

    fix create_user properly

--- task 1/backend/accounts/views.py
+++ task 1/backend/accounts/views.py
@@ -1,18 +1,11 @@
-from django.shortcuts import render
 
-# Create your views here.
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from django.contrib.auth.models import User
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# from rest_framework.permissions import IsAuthenticated
 
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+
 
 class DashboardView(APIView):
     permission_classes = [IsAuthenticated]
@@ -22,10 +15,11 @@ class DashboardView(APIView):
             "message": "Welcome to dashboard!",
             "user": request.user.username
         })
-    
-    @api_view(['GET'])
-    def create_user(request):
-     if not User.objects.filter(username="admin").exists():
+
+
+@api_view(['GET'])
+def create_user(request):
+    if not User.objects.filter(username="admin").exists():
         User.objects.create_user(username="admin", password="admin123")
         return Response({"message": "User created"})
-     return Response({"message": "User already exists"})
+    return Response({"message": "User already exists"})
