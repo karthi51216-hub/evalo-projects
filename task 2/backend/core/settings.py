1f4f7c7853773624059113f3ef728a3576ab181f
Ram Karthi <karthi51216@gmail.com>
2026-04-10 22:27:33 +0530

    temporary fix allowed hosts and cors

--- task 2/backend/core/settings.py
+++ task 2/backend/core/settings.py
@@ -129,6 +129,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "https://evalo-projects-f7s4.vercel.app"
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
